2de6999e01aa5629dd0d43cd500773d07a0a2e4f
daeun <danivalen@naver.com>
2025-01-21 16:18:21 +0900

    :wrench: Config : FE 관리자 페이지 폴더 생성

---
+++ react-project/src/components/admin/AdminHomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-center underline'>
+        관리자 메인 페이지입니다 🎇
+      </h1>
+    </>
+  )
+}
+
+export default Home
---
+++ react-project/src/pages/admin/Admin.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AdminHomePage from '../admin/AdminHomePage'
+
+const Home = () => {
+  return <AdminHomePage />
+}
+
+export default Home
