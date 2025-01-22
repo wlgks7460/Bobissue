7f414f11785e1383d0c62d9f5921faf0f455f2c0
김지한 <wlgks7460@naver.com>
2025-01-22 13:26:28 +0900

    🔧 Config: db 및 jpa설정

---
+++ spring-project/src/main/java/com/c108/springproject/notification/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.c108.springproject.notification.repository;
+
+import com.c108.springproject.notification.domain.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+}
