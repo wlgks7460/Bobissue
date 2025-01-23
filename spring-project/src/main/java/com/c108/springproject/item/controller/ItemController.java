37063214798938841d0adebf09776970d22aa080
hinoyat <hhwj2270@gmail.com>
2025-01-23 14:51:32 +0900

    ✨ Feat: 상품 전체 조회 기능 추가

--- spring-project/src/main/java/com/c108/springproject/item/controller/ItemController.java
+++ spring-project/src/main/java/com/c108/springproject/item/controller/ItemController.java
@@ -1,12 +1,19 @@
 package com.c108.springproject.item.controller;
 
+import com.c108.springproject.global.DefaultResponse;
+import com.c108.springproject.global.ResponseCode;
+import com.c108.springproject.global.dto.ResponseDto;
 import com.c108.springproject.item.domain.Item;
 import com.c108.springproject.item.dto.ItemCreateReqDto;
+import com.c108.springproject.item.dto.ItemCreateResDto;
+import com.c108.springproject.item.dto.ItemReadResDto;
 import com.c108.springproject.item.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RestController
@@ -21,10 +28,16 @@ public class ItemController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<Item> createItem(@RequestBody ItemCreateReqDto itemCreateReqDto) {
-
-        Item item = itemService.createItem(itemCreateReqDto);
-        return ResponseEntity.ok(item);
+    public ResponseEntity<ItemCreateResDto> createItem(@RequestBody ItemCreateReqDto itemCreateReqDto) {  // 매개변수와 반환 타입 수정
+        ItemCreateResDto resDto = itemService.createItem(itemCreateReqDto);
+        return ResponseEntity.ok(resDto);
+    }
+    
+    // 전체 조회
+    @GetMapping("/")
+    public ResponseDto getAllItems() {
+        List<ItemReadResDto> items = itemService.getAllItems();
+        return new ResponseDto(HttpStatus.CREATED, ResponseCode.SUCCESS_CREATE_QUESTION, new DefaultResponse<>(items));
     }
 
 }
--- spring-project/src/main/java/com/c108/springproject/item/service/ItemCategoryService.java
+++ spring-project/src/main/java/com/c108/springproject/item/service/ItemCategoryService.java
@@ -2,12 +2,14 @@ package com.c108.springproject.item.service;
 
 import com.c108.springproject.item.domain.ItemCategory;
 import com.c108.springproject.item.dto.ItemCategoryReqDto;
+import com.c108.springproject.item.dto.ItemCategoryResDto;
 import com.c108.springproject.item.repository.ItemCategoryRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemCategoryService {
@@ -34,10 +36,18 @@ public class ItemCategoryService {
     }
 
     // 카테고리 전체 조회
-    public List<ItemCategory> getAllCategories() {
-        return itemcategoryRepository.findAll();
+    public List<ItemCategoryResDto> getAllCategories() {
+        return itemcategoryRepository.findAll().stream()
+                .map(itemCategory -> ItemCategoryResDto.builder()  // 이 부분에서 ItemCategoryReqDto -> ItemCategoryResDto로 변경
+                        .categoryNo(itemCategory.getCategoryNo())
+                        .name(itemCategory.getName())
+                        .createdAt(itemCategory.getCreatedAt())
+                        .updatedAt(itemCategory.getUpdatedAt())
+                        .build())
+                .collect(Collectors.toList());  // 괄호 수정
     }
-    
+
+
     // id 받아서 카테고리 정보 조회
     public ItemCategory getCategory(int categoryNo) {
         return itemcategoryRepository.findById(categoryNo).orElse(null);
@@ -55,7 +65,7 @@ public class ItemCategoryService {
         ItemCategory updatedCategory = ItemCategory.builder()
                 .categoryNo(category.getCategoryNo())
                 .name(dto.getName())
-                .createdAt(category.getCreatedAt())
+                .createdAt(currentDate)
                 .updatedAt(currentDate)
                 .build();
         return itemcategoryRepository.save(updatedCategory);
