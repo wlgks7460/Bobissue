b5611537573a54d1ca980e8e9fa3e30531feab6e
Hinoyat <hhwj2270@gmail.com>
2025-01-23 21:15:52 +0900

    ✨ Feat: 상품 수정 삭제 추가

--- spring-project/src/main/java/com/c108/springproject/item/controller/ItemController.java
+++ spring-project/src/main/java/com/c108/springproject/item/controller/ItemController.java
@@ -3,13 +3,11 @@ package com.c108.springproject.item.controller;
 import com.c108.springproject.global.DefaultResponse;
 import com.c108.springproject.global.ResponseCode;
 import com.c108.springproject.global.dto.ResponseDto;
-import com.c108.springproject.item.dto.ItemCreateReqDto;
-import com.c108.springproject.item.dto.ItemCreateResDto;
-import com.c108.springproject.item.dto.ItemListResDto;
-import com.c108.springproject.item.dto.ItemResDto;
+import com.c108.springproject.item.dto.*;
 import com.c108.springproject.item.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,18 +31,31 @@ public class ItemController {
         return new ResponseDto(HttpStatus.CREATED, ResponseCode.SUCCESS_CREATE_ITEM, new DefaultResponse<>(resDto));
     }
 
-    // 전체 조회
+    // 상품 전체 조회
     @GetMapping("/")
     public ResponseDto getAllItems() {
         List<ItemListResDto> items = itemService.getAllItems();
         return new ResponseDto(HttpStatus.CREATED, ResponseCode.SUCCESS_CREATE_QUESTION, new DefaultResponse<>(items));
     }
 
-    // 상세 조회
+    // 상품 상세 조회
     @GetMapping("/{itemNo}")
     public ResponseDto getItem(@PathVariable int itemNo) {
         ItemResDto item = itemService.getItem(itemNo);
         return new ResponseDto(HttpStatus.OK, ResponseCode.SUCCESS_FIND_ITEM, new DefaultResponse<>(item));
     }
 
+    // 상품 수정
+    @PutMapping("/{itemNo}")
+    public ResponseDto updateItem(@PathVariable int itemNo, @RequestBody ItemUpdateReqDto reqDto) {
+        ItemUpdateResDto updatedItem = itemService.updateItem(itemNo, reqDto);
+        return new ResponseDto(HttpStatus.OK, ResponseCode.SUCCESS_UPDATE_ITEM, new DefaultResponse<>(updatedItem));
+    }
+    
+    // 상품 삭제
+    @DeleteMapping("/{itemNo}")
+    public ResponseEntity<Void> deleteItem(@PathVariable int itemNo) {
+        itemService.deleteItem(itemNo);
+        return ResponseEntity.noContent().build();
+    }
 }
