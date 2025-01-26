22f26699f6ed1e8cd889bb5a8356e689be8f8395
daeun <danivalen@naver.com>
2025-01-26 17:39:32 +0900

    :iphone: Design : 관리자페이지 CS 관리 파트 기본 구성 완료

---
+++ react-project/src/pages/admin/cs/FAQPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FAQ from '../../../components/admin/cs/FAQ'
+
+const FAQPage = () => {
+  return (
+    <div className='ml-64 mt-16 p-4'>
+      <FAQ />
+    </div>
+  )
+}
+export default FAQPage
