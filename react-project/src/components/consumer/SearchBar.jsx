a5bcab4889c92c2953fed4d1b83c305ca28f0c5f
KimJiwon <rkl45687@gmail.com>
2025-02-03 10:29:54 +0900

    :sparkles: Feat: 검색 결과 페이지 S12P11C108-96 - searchbar 구현 중

--- react-project/src/components/consumer/SearchBar.jsx
+++ react-project/src/components/consumer/SearchBar.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { ShoppingBagIcon } from '@heroicons/react/24/solid'
+import { ShoppingBagIcon, Bars3Icon } from '@heroicons/react/24/solid'
 
 const SearchBar = () => {
+  const [categories, setCategories] = useState([])
+
+  // 카테고리 show
+  const [showCategory, setShowCategory] = useState(false)
+
+  useEffect(() => {
+    setCategories([
+      { categoryNo: 0, name: '닭가슴살' },
+      { categoryNo: 1, name: '포케' },
+      { categoryNo: 2, name: '도시락' },
+    ])
+  }, [])
   return (
-    <div className='w-full p-3 border flex justify-between sticky z-10 top-0 bg-slate-100'>
-      <div>
-        <p>카테고리</p>
-      </div>
-      <div>searchBar</div>
-      <div className='flex'>
-        <Link to={'/cart'}>
-          <ShoppingBagIcon className='w-5' />
-        </Link>
+    <div className='w-full p-3 border flex justify-center sticky z-10 top-0 bg-slate-100'>
+      <div
+        className='w-[70rem] flex justify-between items-center gap-3'
+        onMouseOver={() => setShowCategory(true)}
+        onMouseOut={() => setShowCategory(false)}
+      >
+        <div className='w-[100px] flex-none flex items-center gap-2 relative'>
+          <Bars3Icon className='w-6' />
+          <span>카테고리</span>
+          {showCategory && (
+            <div className='absolute -bottom-[30px]'>
+              {categories.map((v) => (
+                <Link>{v.name}</Link>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className='grow flex justify-center'>
+          <input type='text' className='w-[500px] h-[50px] border border-indigo-400 rounded' />
+        </div>
+        <div className='w-[100px] flex-none flex justify-end'>
+          <Link to={'/cart'}>
+            <ShoppingBagIcon className='w-5' />
+          </Link>
+        </div>
       </div>
     </div>
   )
