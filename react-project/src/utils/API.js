784b4949910dc62a79fd5f61df9896adbe17522b
KimJiwon <rkl45687@gmail.com>
2025-02-04 13:37:03 +0900

    :wrench: Fix: accessToken 갱신 S12P11C108-238

--- react-project/src/utils/API.js
+++ react-project/src/utils/API.js
@@ -12,12 +12,11 @@ const API = axios.create({
 
 // 요청 interceptors: 요청에 jwt 추가
 API.interceptors.request.use((config) => {
-  const access_token = store.getState().user.accessToken
-  const refresh_token = store.getState().user.refreshToken
-  if (access_token) {
-    console.log('토큰 있음', access_token)
-    config.headers.Authorization = `Bearer ${access_token}`
-    config.headers['refreshToken'] = `Bearer ${refresh_token}`
+  const { accessToken, refreshToken } = store.getState().user
+  if (accessToken) {
+    console.log('토큰 있음', accessToken)
+    config.headers.Authorization = `Bearer ${accessToken}`
+    config.headers['refreshToken'] = `Bearer ${refreshToken}`
   }
 
   return config
@@ -27,10 +26,9 @@ API.interceptors.request.use((config) => {
 API.interceptors.response.use((res) => {
   // access_token 갱신
   console.log(res)
-  const newAccessToken = res.headers.get('newAccessToken')
+  const newAccessToken = res.headers['newaccesstoken']
   console.log(newAccessToken)
-  if (res.headers['authorization']) {
-    const newAccessToken = res.headers['authorization']
+  if (newAccessToken) {
     store.dispatch(
       userReducerActions.login({
         access_token: newAccessToken,
