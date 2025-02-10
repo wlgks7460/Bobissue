992266e5ca6ee392239e43b8d3ee013d4ecc9d52
KimJiwon <rkl45687@gmail.com>
2025-02-10 10:48:19 +0900

    :sparkles Feat: 고객센터 기본구성 S12P11C108 - 사이드바, Footer 고객센터 관련 Link

--- react-project/src/components/consumer/home/HomeItemList.jsx
+++ react-project/src/components/consumer/home/HomeItemList.jsx
@@ -1,28 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ItemCard from '../common/ItemCard'
 import API from '../../../utils/API'
 
 const HomeItemList = ({ category }) => {
-  const [items, setItems] = useState([]) // 상품 데이터 리스트
-  // 카테고리에 맞는 상품 불러오기
-  useEffect(() => {
-    // mount
+  // 화면 감지 관련
+  const divRef = useRef(null)
+  const [hasFetched, setHasFetched] = useState(false)
+
+  // 데이터 불러오기
+  const getCategoryItem = () => {
     API.get(`/categories/${category.categoryNo}`)
       .then((res) => {
         console.log(res)
         const result = res.data.result?.data.items
         if (result) {
           setItems(result)
+          setHasFetched(true)
         }
       })
       .catch((err) => {
         console.error(err)
       })
+  }
+
+  const [items, setItems] = useState([]) // 상품 데이터 리스트
+  // 카테고리에 맞는 상품 불러오기
+  useEffect(() => {
+    // mount
+    if (hasFetched) return // 이미 실행되었으면 추가 실행 방지지
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          getCategoryItem()
+        }
+      },
+      { threshold: 0.5 }, // 50% 이상 보여야 감지
+    )
+
+    if (divRef.current) {
+      observer.observe(divRef.current)
+    }
     // unmount
-    return () => {}
+    return () => {
+      observer.disconnect()
+    }
   }, [])
   return (
-    <div className='w-[70rem]'>
+    <div className='w-[70rem]' ref={divRef}>
       <div className='flex justify-between mb-5'>
         <p className='text-xl'>{category.name}</p>
         <button>+ 더보기</button>
