10a2fc6a71bcb2327f32e870a363da00af9edf75
KimJiwon <rkl45687@gmail.com>
2025-02-12 01:36:54 +0900

    :sparkles: Feat: 마이페이지 - 내 레시피 S12P11C108-333

--- react-project/src/pages/RootPage.jsx
+++ react-project/src/pages/RootPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import ScrollToTop from '../components/common/ScrollToTop'
-import LogoutWatcher from '../components/common/LogoutWatcher'
 
 const RootPage = () => {
   return (
     <>
-      <LogoutWatcher />
       <ScrollToTop />
       <Outlet />
     </>
