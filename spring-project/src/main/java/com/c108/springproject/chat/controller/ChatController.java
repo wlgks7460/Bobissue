5d74208919188118d64f954f356a4ffe3115080c
김지한 <wlgks7460@naver.com>
2025-02-13 17:58:54 +0900

    ✨ Feat : 라이브 커머스

--- spring-project/src/main/java/com/c108/springproject/chat/controller/ChatController.java
+++ spring-project/src/main/java/com/c108/springproject/chat/controller/ChatController.java
@@ -4,9 +4,12 @@ import com.c108.springproject.chat.dto.ChatMessageDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class ChatController {
@@ -20,9 +23,10 @@ public class ChatController {
 
     @MessageMapping("/messages")
     //@DestinationVariable에 추가로 chattingRoomId를 괄호 쓰고 넣어줘야 파라미터 인식을 함.
-    public ChatMessageDto send2( @RequestBody ChatMessageDto chatMessageDto) {
+    public ChatMessageDto send2( @RequestBody ChatMessageDto chatMessageDto, Principal principal) {
+        System.out.println(principal.getName());
         System.out.println(chatMessageDto.getContent());
-        template.convertAndSend("/sub/message", chatMessageDto.getContent());       // 구독중인 모든 사용자에게 메시지를 전달합니다.
+        template.convertAndSend("/sub/message", chatMessageDto);       // 구독중인 모든 사용자에게 메시지를 전달합니다.
         return chatMessageDto;
     }
 }
