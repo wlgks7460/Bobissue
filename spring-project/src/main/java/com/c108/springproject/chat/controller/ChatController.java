7aa7e2776fe2824771479953b65e676be30740cc
wlgks7460 <wlgks7460@naver.com>
2025-02-14 11:51:54 +0900

    ✨ Feat : Openvidu 설정

--- spring-project/src/main/java/com/c108/springproject/chat/controller/ChatController.java
+++ spring-project/src/main/java/com/c108/springproject/chat/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.c108.springproject.chat.controller;
 
 import com.c108.springproject.chat.dto.ChatMessageDto;
+import com.c108.springproject.chat.dto.SignalMessageDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -23,10 +24,10 @@ public class ChatController {
 
     @MessageMapping("/messages")
     //@DestinationVariable에 추가로 chattingRoomId를 괄호 쓰고 넣어줘야 파라미터 인식을 함.
-    public ChatMessageDto send2( @RequestBody ChatMessageDto chatMessageDto, Principal principal) {
-        System.out.println(principal.getName());
+    public ChatMessageDto send2( @RequestBody ChatMessageDto chatMessageDto) {
         System.out.println(chatMessageDto.getContent());
         template.convertAndSend("/sub/message", chatMessageDto);       // 구독중인 모든 사용자에게 메시지를 전달합니다.
         return chatMessageDto;
     }
+
 }
