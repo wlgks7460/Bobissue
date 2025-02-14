686218115953d3ed2fe3541e5ccf14eeb675d96a
KimJiwon <rkl45687@gmail.com>
2025-02-14 11:47:49 +0900

    :bug: Fix: 마이페이지 - 식단 관리 수정 - 일일 권장 칼로리 계산 - 식단 이미지 추가 - 일일 권장 칼로리 이상의 칼로리 섭취시 캘린더에 경고등 표시

--- react-project/src/components/consumer/mypage/MyPageCalendarModalItem.jsx
+++ react-project/src/components/consumer/mypage/MyPageCalendarModalItem.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import dayjs from 'dayjs'
+import calendarDefaultImg from '../../../assets/consumer/calendarDefault.webp'
 
 const MyPageCalendarModalItem = ({ meal, updateData, deleteData }) => {
   const [mode, setMode] = useState('read')
@@ -7,10 +8,31 @@ const MyPageCalendarModalItem = ({ meal, updateData, deleteData }) => {
   const nameRef = useRef()
   const timeRef = useRef()
   const calorieRef = useRef()
+  const [mealImage, setMealImage] = useState(null)
 
-  const [hour, setHour] = useState(dayjs(meal.eatTime).hour())
-  const [minute, setMinute] = useState(dayjs(meal.eatTime).minute())
+  // 시간 데이터 가공
+  const handleTime = (timeData) => {
+    const date = timeData.split(' ')[0]
+    const time = timeData.split(' ').pop()
+    return `${date} ${time}`
+  }
+  const [hour, setHour] = useState(dayjs(handleTime(meal.eatTime)).hour())
+  const [minute, setMinute] = useState(dayjs(handleTime(meal.eatTime)).minute())
 
+  // 이미지 파일 핸들러
+  const handleImageChange = (e) => {
+    const file = e.target.files[0] // 첫 번째 파일 선택
+    if (file) {
+      setMealImage(file)
+    }
+  }
+
+  // 시간 출력
+  const handleHour = (hour) => {
+    const result = hour > 13 ? hour - 12 : hour === 0 ? hour + 12 : hour
+    return String(result).padStart(2, '0')
+  }
+  // 식단 수정 핸들러러
   const handleUpdate = (e) => {
     e.preventDefault()
     updateData(
@@ -18,10 +40,11 @@ const MyPageCalendarModalItem = ({ meal, updateData, deleteData }) => {
       timeRef.current.value,
       calorieRef.current.value,
       meal.calendarNo,
+      mealImage,
     )
     setMode('read')
   }
-
+  // 식단 삭제 핸들러
   const handleDelete = (e) => {
     e.preventDefault()
     deleteData(meal.calendarNo)
@@ -33,15 +56,20 @@ const MyPageCalendarModalItem = ({ meal, updateData, deleteData }) => {
         <div className='flex items-center space-x-4 mb-4'>
           <div className='border border-gray-300 rounded'>
             <img
-              src={meal.image}
+              src={meal.images[0]?.imageUrl || calendarDefaultImg}
               alt={meal.title}
               className='w-[80px] h-[80px] object-cover rounded'
+              onError={(e) => {
+                e.target.src = calendarDefaultImg
+              }}
             />
           </div>
           <div>
             <h4 className='font-bold'>{meal.name}</h4>
             <div>
-              <span className='me-5'>{`${hour < 12 ? '오전' : '오후'} ${hour > 13 ? hour - 12 : hour === 0 ? hour + 12 : hour}:${minute}`}</span>
+              <span className='me-5'>
+                {`${hour < 12 ? '오전' : '오후'} ${handleHour(hour)}:${String(minute).padStart(2, '0')}`}
+              </span>
               <span className='text-sm text-gray-500'>{meal.calorie} kcal</span>
             </div>
             <div className='flex gap-3'>
@@ -60,9 +88,16 @@ const MyPageCalendarModalItem = ({ meal, updateData, deleteData }) => {
           <div className='flex items-center space-x-4 mb-1'>
             <div className='border border-gray-300 rounded'>
               <img
-                src={meal.image}
+                src={
+                  (mealImage && URL.createObjectURL(mealImage)) ||
+                  meal.images[0]?.imageUrl ||
+                  calendarDefaultImg
+                }
                 alt={meal.title}
                 className='w-[80px] h-[80px] object-cover rounded'
+                onError={(e) => {
+                  e.target.src = calendarDefaultImg
+                }}
               />
             </div>
             <div>
@@ -90,7 +125,7 @@ const MyPageCalendarModalItem = ({ meal, updateData, deleteData }) => {
                 </span>
               </div>
               <div className='flex gap-3'>
-                <input type='file' name='' id='' accept='image/*' />
+                <input type='file' name='' id='' accept='image/*' onChange={handleImageChange} />
                 <button className='p-1 text-indigo-600' onClick={handleUpdate}>
                   수정
                 </button>
