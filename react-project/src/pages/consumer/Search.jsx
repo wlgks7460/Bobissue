5a4b3164652acd54690021d7084c22efa9c54464
KimJiwon <rkl45687@gmail.com>
2025-02-14 12:15:50 +0900

    :bug: Fix: 검색 결과 페이지 수정

--- react-project/src/pages/consumer/Search.jsx
+++ react-project/src/pages/consumer/Search.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import SearchBar from '../../components/consumer/common/SearchBar'
 import SearchItemList from '../../components/consumer/itemList/SearchItemList'
+import API from '../../utils/API'
 
 const Search = () => {
   // 쿼리스트링
   const [searchParams, setSearchParams] = useSearchParams()
 
+  // 검색 데이터 가져오기
+  const getSearchResult = (page = 1) => {
+    const payload = {
+      search: searchParams.get('keyword'),
+      page: page,
+    }
+    console.log(payload)
+    // API.get('/search', payload)
+    //   .then((res) => {
+    //     console.log(res)
+    //   })
+    //   .catch((err) => {
+    //     console.error(err)
+    //   })
+  }
+  useEffect(() => {
+    getSearchResult(1)
+  }, [])
   return (
     <div>
       <SearchBar />
