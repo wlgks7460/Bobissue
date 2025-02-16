3a45fbf56b774c2f411fbfc9e9f76ca43bbb9672
daeun <danivalen@naver.com>
2025-02-16 20:39:38 +0900

    :iphone: Design : 관리자페이지 - 대시보드 - 카운팅 애니메이션 추가

--- react-project/src/components/admin/home/AdminDashboard.jsx
+++ react-project/src/components/admin/home/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import API from '../../../utils/API'
+import CountUp from 'react-countup'
 import {
   ShoppingCartIcon,
   UsersIcon,
@@ -62,24 +63,38 @@ const AdminDashBoard = () => {
 
   // 더미 데이터 생성
   const dummyOrders = [
-    { orderNumber: '20240101', customer: '홍길동', amount: 50000, date: '2024-02-15' },
-    { orderNumber: '20240102', customer: '김철수', amount: 70000, date: '2024-02-14' },
-    { orderNumber: '20240103', customer: '이영희', amount: 65000, date: '2024-02-13' },
+    { orderNumber: '20240101', customer: '김지한', amount: 50000, date: '2024-02-15' },
+    { orderNumber: '20240102', customer: '김지원', amount: 70000, date: '2024-02-14' },
+    { orderNumber: '20240103', customer: '이다은', amount: 65000, date: '2024-02-13' },
+    { orderNumber: '20240101', customer: '강현호', amount: 50000, date: '2024-02-15' },
+    { orderNumber: '20240102', customer: '윤경상', amount: 70000, date: '2024-02-14' },
+    { orderNumber: '20240103', customer: '김경은', amount: 65000, date: '2024-02-13' },
+    { orderNumber: '20240103', customer: '손흥민', amount: 65000, date: '2024-02-13' },
   ]
 
   const dummyUsers = [
-    { name: '홍길동', email: 'hong@example.com', dateJoined: '2024-02-01' },
+    { name: '김지한', email: 'hong@example.com', dateJoined: '2024-02-01' },
     { name: '김철수', email: 'kim@example.com', dateJoined: '2024-01-25' },
-    { name: '이영희', email: 'lee@example.com', dateJoined: '2024-01-20' },
+    { name: '이다은', email: 'lee@example.com', dateJoined: '2024-01-20' },
+    { name: '강현호', email: 'hong@example.com', dateJoined: '2024-02-01' },
+    { name: '김철수', email: 'kim@example.com', dateJoined: '2024-01-25' },
+    { name: '윤경상', email: 'lee@example.com', dateJoined: '2024-01-20' },
+    { name: '김경은', email: 'lee@example.com', dateJoined: '2024-01-20' },
   ]
 
   return (
     <div>
-      {/* <h1 className='text-2xl font-bold text-gray-800 mb-6'>📊 관리자 대시보드</h1> */}
+      {/* 대시보드 메인 총 매출 카운트업 */}
+      <div className='mb-4 text-center p-4 rounded-xl shadow-md bg-gradient-to-r from-[#5a4630] to-[#725a3e] animate-gradient-x h-32 flex items-center justify-center'>
+        <h3 className='text-4xl font-bold text-white mr-4'>⚡ 이번달 총 매출</h3>
+        <p className='text-4xl font-extrabold text-yellow-300'>
+          <CountUp start={0} end={totalSales || 0} duration={2.5} separator=',' /> 원
+        </p>
+      </div>
 
       {/* 주요 통계 카드 */}
-      <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 mb-6'>
-        <div className='bg-[#FDF6E3] shadow-md rounded-lg p-5 flex flex-col items-start'>
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 mb-4'>
+        <div className='bg-[#FDF6E3] shadow-md rounded-lg p-4 flex flex-col items-start'>
           <div className='flex items-center space-x-2 mb-2'>
             <ShoppingCartIcon className='h-6 w-6 text-[#5C4033]' />
             <h2 className='text-lg font-semibold text-gray-800'>총 매출</h2>
@@ -89,7 +104,7 @@ const AdminDashBoard = () => {
           </p>
         </div>
 
-        <div className='bg-[#FAEBD7] shadow-md rounded-lg p-5 flex flex-col items-start'>
+        <div className='bg-[#FAEBD7] shadow-md rounded-lg p-4 flex flex-col items-start'>
           <div className='flex items-center space-x-2 mb-2'>
             <UsersIcon className='h-6 w-6 text-[#5C4033]' />
             <h2 className='text-lg font-semibold text-gray-800'>총 유저 수</h2>
@@ -101,7 +116,7 @@ const AdminDashBoard = () => {
           </p>
         </div>
 
-        <div className='bg-[#FFF5E1] shadow-md rounded-lg p-5 flex flex-col items-start'>
+        <div className='bg-[#FFF5E1] shadow-md rounded-lg p-4 flex flex-col items-start'>
           <div className='flex items-center space-x-2 mb-2'>
             <ClipboardDocumentListIcon className='h-6 w-6 text-[#5C4033]' />
             <h2 className='text-lg font-semibold text-gray-800'>판매자 회사 수</h2>
@@ -111,7 +126,7 @@ const AdminDashBoard = () => {
           </p>
         </div>
 
-        <div className='bg-[#FDF5E6] shadow-md rounded-lg p-5 flex flex-col items-start'>
+        <div className='bg-[#FDF5E6] shadow-md rounded-lg p-4 flex flex-col items-start'>
           <div className='flex items-center space-x-2 mb-2'>
             <StarIcon className='h-6 w-6 text-[#5C4033]' />
             <h2 className='text-lg font-semibold text-gray-800'>이번주 베스트 상품(예시)</h2>
@@ -120,52 +135,53 @@ const AdminDashBoard = () => {
         </div>
       </div>
 
-      {/* 최근 주문 내역 */}
-      <div className='bg-white hover:shadow-md rounded-lg p-5 mb-6'>
-        <h2 className='text-xl font-bold text-[#5C4033] mb-3'>🛒 최근 주문 내역</h2>
-        <table className='w-full border border-[#D2B48C]'>
-          <thead className='bg-[#FAEBD7] text-gray-700'>
-            <tr>
-              <th className='border border-[#D2B48C] p-2'>주문번호</th>
-              <th className='border border-[#D2B48C] p-2'>고객명</th>
-              <th className='border border-[#D2B48C] p-2'>주문금액</th>
-              <th className='border border-[#D2B48C] p-2'>주문일자</th>
-            </tr>
-          </thead>
-          <tbody>
-            {dummyOrders.map((order, index) => (
-              <tr key={index} className='text-center hover:bg-[#FFF5E1]'>
-                <td className='border border-[#D2B48C] p-2'>{order.orderNumber}</td>
-                <td className='border border-[#D2B48C] p-2'>{order.customer}</td>
-                <td className='border border-[#D2B48C] p-2'>₩ {order.amount.toLocaleString()}</td>
-                <td className='border border-[#D2B48C] p-2'>{order.date}</td>
+      {/* 최근 주문 내역 및 최근 회원 가입 나란히 배치 */}
+      <div className='grid grid-cols-2 gap-4'>
+        <div className='bg-white hover:shadow-md rounded-lg p-4'>
+          <h2 className='text-xl font-bold text-[#5C4033] mb-3'>🛒 최근 주문 내역</h2>
+          <table className='w-full border border-[#D2B48C]'>
+            <thead className='bg-[#FAEBD7] text-gray-700'>
+              <tr>
+                <th className='border border-[#D2B48C] p-2'>주문번호</th>
+                <th className='border border-[#D2B48C] p-2'>고객명</th>
+                <th className='border border-[#D2B48C] p-2'>주문금액</th>
+                <th className='border border-[#D2B48C] p-2'>주문일자</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {dummyOrders.map((order, index) => (
+                <tr key={index} className='text-center hover:bg-[#FFF5E1]'>
+                  <td className='border border-[#D2B48C] p-2'>{order.orderNumber}</td>
+                  <td className='border border-[#D2B48C] p-2'>{order.customer}</td>
+                  <td className='border border-[#D2B48C] p-2'>₩ {order.amount.toLocaleString()}</td>
+                  <td className='border border-[#D2B48C] p-2'>{order.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
-      {/* 최근 회원 가입 */}
-      <div className='bg-white hover:shadow-md rounded-lg p-5'>
-        <h2 className='text-xl font-bold text-[#5C4033] mb-3'>🙋‍♂️ 최근 회원 가입</h2>
-        <table className='w-full border border-[#D2B48C]'>
-          <thead className='bg-[#FAEBD7] text-gray-700'>
-            <tr>
-              <th className='border border-[#D2B48C] p-2'>이름</th>
-              <th className='border border-[#D2B48C] p-2'>이메일</th>
-              <th className='border border-[#D2B48C] p-2'>가입일자</th>
-            </tr>
-          </thead>
-          <tbody>
-            {dummyUsers.map((user, index) => (
-              <tr key={index} className='text-center hover:bg-[#FFF5E1]'>
-                <td className='border border-[#D2B48C] p-2'>{user.name}</td>
-                <td className='border border-[#D2B48C] p-2'>{user.email}</td>
-                <td className='border border-[#D2B48C] p-2'>{user.dateJoined}</td>
+        <div className='bg-white hover:shadow-md rounded-lg p-4'>
+          <h2 className='text-xl font-bold text-[#5C4033] mb-3'>🙋‍♂️ 최근 회원 가입</h2>
+          <table className='w-full border border-[#D2B48C]'>
+            <thead className='bg-[#FAEBD7] text-gray-700'>
+              <tr>
+                <th className='border border-[#D2B48C] p-2'>이름</th>
+                <th className='border border-[#D2B48C] p-2'>이메일</th>
+                <th className='border border-[#D2B48C] p-2'>가입일자</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {dummyUsers.map((user, index) => (
+                <tr key={index} className='text-center hover:bg-[#FFF5E1]'>
+                  <td className='border border-[#D2B48C] p-2'>{user.name}</td>
+                  <td className='border border-[#D2B48C] p-2'>{user.email}</td>
+                  <td className='border border-[#D2B48C] p-2'>{user.dateJoined}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
