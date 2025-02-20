9d6344f09961fa9bad5e87c3b2b43381a8a127ab
김지한 <wlgks7460@naver.com>
2025-02-20 16:15:44 +0900

    fix : elasticsearch

--- spring-project/src/main/java/com/c108/springproject/SpringProjectApplication.java
+++ spring-project/src/main/java/com/c108/springproject/SpringProjectApplication.java
@@ -2,6 +2,7 @@ package com.c108.springproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
